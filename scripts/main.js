d622fbd86025fa6e6d848bdb6dcf9340ab774cb1
tuningmind <tuningmind@gmail.com>
2017-03-10 20:57:13 -0700

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+var words = [
+  "the","of","and","a","to","in","is","you","that","it","he",
+  "was","for","on","are","as","with","his","they","I","at","be",
+  "this","have","from","or","one","had","by","word","but","not",
+  "what","all","were","we","when","your","can","said","there",
+  "use","an","each","which","she","do","how","their","if","will",
+  "up","other","about","out","many","then","them","these","so",
+  "some","her","would","make","like","him","into","time","has",
+  "look","two","more","write","go","see","number","no","way",
+  "could","people","my","than","first","water","been","call",
+  "who","oil","its","now","find","long","down","day","did","get",
+  "come","made","may","part"
+];
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const word = words[getRandomIntInclusive(0, words.length)];
+
+let elems = document.querySelector("div");
+elem[0].textContent = word;
+elem[3].textContent = word;
+
+
