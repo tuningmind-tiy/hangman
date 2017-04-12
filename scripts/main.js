76b9d7e5e3e5796c49d4936309a362670a35dcc5
tuningmind <tuningmind@gmail.com>
2017-04-12 14:52:32 -0600

    Refactor experiments

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
+
 window.onload = function () {
 
     let letter = '';
     let unders = '';
     let used = [];
     let turns = 20;
-    let alphabet = {
+    let wordnode = document.querySelector("div#word");
+    let inpt = document.querySelector('input#inpt');
+    let msg = document.querySelector('div#msg');
+    let alphabet= {
       'a': document.querySelector('p#a'),
       'b': document.querySelector('p#b'),
       'c': document.querySelector('p#c'),
@@ -24,7 +28,7 @@ window.onload = function () {
       'q': document.querySelector('p#q'),
       'r': document.querySelector('p#r'),
       's': document.querySelector('p#s'),
-      't': document.querySelector('p#t'),
+      't': document.querySelector('p#y'),
       'u': document.querySelector('p#u'),
       'v': document.querySelector('p#v'),
       'w': document.querySelector('p#w'),
@@ -32,35 +36,6 @@ window.onload = function () {
       'y': document.querySelector('p#y'),
       'z': document.querySelector('p#z')
     }
-    let wordnode = document.querySelector("div#word");
-      let anode = document.querySelector('p#a');
-      let bnode = document.querySelector('p#b');
-      let cnode = document.querySelector('p#c');
-      let dnode = document.querySelector('p#d');
-      let enode = document.querySelector('p#e');
-      let fnode = document.querySelector('p#f');
-      let gnode = document.querySelector('p#g');
-      let hnode = document.querySelector('p#h');
-      let inode = document.querySelector('p#i');
-      let jnode = document.querySelector('p#j');
-      let knode = document.querySelector('p#k');
-      let lnode = document.querySelector('p#l');
-      let mnode = document.querySelector('p#m');
-      let nnode = document.querySelector('p#n');
-      let onode = document.querySelector('p#o');
-      let pnode = document.querySelector('p#p');
-      let qnode = document.querySelector('p#q');
-      let rnode = document.querySelector('p#r');
-      let snode = document.querySelector('p#s');
-      let tnode = document.querySelector('p#t');
-      let unode = document.querySelector('p#u');
-      let vnode = document.querySelector('p#v');
-      let wnode = document.querySelector('p#w');
-      let xnode = document.querySelector('p#x');
-      let ynode = document.querySelector('p#y');
-      let znode = document.querySelector('p#z');
-    let inpt = document.querySelector('input#inpt');
-    let msg = document.querySelector('div#msg');
     function getRandomIntInclusive(min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
@@ -68,8 +43,6 @@ window.onload = function () {
     }
     const word = words[getRandomIntInclusive(0, words.length)];
     unders = word.split('').map(function(charact){return "_"}).join('');
-    console.log("The random word is '", word, "'");
-
     wordnode.textContent = unders;
   //----------------
     function makeUnders(word, letter) {
@@ -89,35 +62,45 @@ window.onload = function () {
     function isLetterInWord (word, letter) {
       return (word.indexOf(letter) > -1)
     }
-  //----------------
-  inpt.addEventListener('change', function() {
-    if (turns === 0 ) {
-      msg.textContent = "game over, no win this time"
+    function endGame() {
       inpt.value = '';
-      // display correct word
-      return wordnode.textContent = word; 
-      // if the guessed letter is in the used array
-    } else if (used.indexOf(inpt.value) > -1) {
-      // clear the guessed letter
-      inpt.value = '';
-      msg.textContent = "You already used that letter"; 
-    } else {
-      // the guessed letter has not been used
-      // and there are turns remaining
-      // clear any previous messages on the screen
-      msg.textContent = '';
-      // grab the guessed letter
-      letter = inpt.value;
-      isLetterInWord(word, letter) ? turns = turns : turns -= 1;
+      msg.textContent = "Game over. The word was"
+      wordnode.textContent = word; 
+    }
+    function tryAnother() {
+      inpt.textContent= '';
+      msg.textContent = "You already used that letter. Try another one."; 
+    }
+    function updateUnders(letter) {
+      turns -= 1  
       used.push(letter);
       unders = makeUnders(word, letter);
       wordnode.textContent = unders;
-      alphabet[letter].style.color = "#141414";
+      alphabet[letter].style.color = "black";
       inpt.value = '';
       msg.textContent = turns + " turns remaining"
-      if (unders === word) {
-        msg.textContent = "You won!";
+    }
+    function announceWin() {
+      msg.textContent = "You won!";
+      msg.style.color = "magenta";
+    }
+
+  //----------------
+  inpt.addEventListener('input', function() {
+    letter = inpt.value;
+    let usedLetter = used.indexOf(letter) >= 0
+    let wordGuessed = (word === unders)
+    let goodGuess = isLetterInWord(word, letter)
+    if (turns) {
+      if (usedLetter){ tryAnother() } 
+      else if (wordGuessed) { announceWin() } 
+      else if (goodGuess) { updateUnders(letter) }
+      else {
+        msg.textContent = ''
+        turns -= 1 
       }
+    } else {
+      endGame()
     }
   })
 }
